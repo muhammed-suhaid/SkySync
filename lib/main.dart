ac181d389c866fbd3a9e1736e01383722eccab78
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2025-02-19 21:20:37 +0530

    Created a weather page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skysync/Screens/weather_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'SkySync',
       debugShowCheckedModeBanner: false,
-      //home:,
+      home:WeatherPage(),
     );
   }
 }
