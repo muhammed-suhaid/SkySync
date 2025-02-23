ffba7c81239bfa485891e08fe86358a63e72c79c
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2025-02-23 10:23:48 +0530

    Created a color manager

---
+++ lib/resources/color_manager.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color white = Colors.white;
+
+  static const Color sunny = Color(0xFFFAE2BD);
+  static const Color sunnytxt = Color(0xFFEFAA82);
+
+  static const Color snowy = Color(0xFF99B8CC);
+  static const Color snowytxt = Color(0xFFE4F1F9);
+
+  static const Color rainy = Color(0xFF7FC3AE);
+  static const Color rainytxt = Color(0xFFC9E8E0);
+
+  static const Color cloudy = Color(0xFF91B4C6);
+  static const Color cloudytxt = Color(0xFFCAD7DF);
+}
